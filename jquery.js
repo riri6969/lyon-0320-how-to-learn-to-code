622474815f9e983b588a54cd1bc52425c9023c68
Pakorek <brice.darmenia@sfr.fr>
2020-03-13 02:00:34 +0100

    On est plutôt pas mal

---
+++ jquery.js
@@ -0,0 +1,14 @@
+$(function() {
+	const main = $('#pako_main');
+
+    main.hide();
+
+	$('.button_pako_front').click(() => {
+		main.show();
+		// d.show();
+		// main.css('transform', 'scale(1)');
+        // d.css('margin', '25px 0');
+    }
+});
+
+
